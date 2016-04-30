c114fe80b81780fd55af016d477efb4ad969a187
sealtiel92 <sealtiel92@users.noreply.github.com>
2016-04-30 00:57:55 -0500

    validation

--- parametros.cpp
+++ parametros.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(int args, char *argv[]){
+	if(args > 1)
+	{
 		string cp(argv[1]);
-		if(cp == "-f"){
+		if(cp == "-f")
+		{
 			cout<<"modificador: "<<cp<<endl;
 		}
+	}
 	return 0;
 }
